c6de079e3751d28a71802770d33b2351a6e64289
liuxiany <liuxiany@us.ibm.com>
2016-07-15 18:21:47 -0700

    fix x==MIN_INT issue, use (1<<31)-1 as MAX_INT, remove digit<=MAX_INT%10 which will not happen for 32 bit integer

--- hudeven/P007.py
+++ hudeven/P007.py
@@ -4,13 +4,15 @@ class Solution(object):
         :type x: int
         :rtype: int
         """
-        MAX_INT = 2147483648
-        MIN_INT = -2147483647
-        res = 0;
+        MAX_INT = (1 << 31) - 1
+        MIN_INT = -MAX_INT - 1
+        if x == MIN_INT: return 0
+        
+        res = 0
         p = x if x >= 0 else -x
         while p != 0:
             digit = p % 10
-            if res < MAX_INT / 10 or ( res == MAX_INT / 10 and digit <= MAX_INT % 10):
+            if res <= MAX_INT / 10:
                 res = res * 10 + digit
             else:
                 return 0
