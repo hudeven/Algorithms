0b3e55734946a46b69671a9fdb7d9b07aaa697d3
updownlife <xyliu.365@gmail.com>
2016-07-16 10:26:09 -0700

    add commit "need to check if i + 10 >= len(s) in other language"

--- hudeven/P187.py
+++ hudeven/P187.py
@@ -9,6 +9,7 @@ class Solution(object):
         if s == None or len(s) < 10:    return list(res)
 
         for i in range(len(s)):
+            # need to check if i + 10 >= len(s) in other language
             sub = s[i : i + 10]
             if sub in visited :
                 res.add(sub)
